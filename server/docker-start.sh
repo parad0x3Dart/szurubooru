2f464454aaac03cb7a9d0391f2df57bed0c7210b
parad0x3D <dev@parad0x3d.art>
2024-10-29 07:58:21 -0400

    server/docker: Fix permissions before dropping to app user

--- server/docker-start.sh
+++ server/docker-start.sh
@@ -2,7 +2,17 @@
 set -e
 cd /opt/app
 
-alembic upgrade head
+if [ -f "/config.yaml" ]; then
+    echo "Using config.yaml from /"
+    cp /config.yaml /opt/app/config.yaml
+    chmod 444 /opt/app/config.yaml
+    chown app:app /opt/app/config.yaml
+fi
+
+chown -R app:app /data/temporary-uploads
+
+echo "Running database migrations"
+su-exec app alembic upgrade head
 
 echo "Starting szurubooru API on port ${PORT} - Running on ${THREADS} threads"
-exec waitress-serve-3 --port ${PORT} --threads ${THREADS} szurubooru.facade:app
+exec su-exec app waitress-serve-3 --port ${PORT} --threads ${THREADS} szurubooru.facade:app
